9abd2889d5b74721e398a0262cdab6192a055abc
Weekend Master <weekend@super.com>
2017-11-07 13:04:54 +0300

    cleaned handlers

--- src/App.js
+++ src/App.js
@@ -5,23 +5,6 @@ class App extends React.Component {
 
     constructor() {
         super();
-
-        this.state = {
-            value: 1239002,
-            yell:  "YELL!"
-        };
-
-        this.setStateHandler = this.setStateHandler.bind(this);
-        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-    }
-
-    setStateHandler() {
-        var v = this.state.value;
-        this.setState({ value: v + 1 });
-    }
-
-    forceUpdateHandler() {
-        this.forceUpdate();
     }
 
     render() {
