456ebe3bf8a1336145f4182f323722f27622584c
Weekend Master <weekend@super.com>
2017-11-08 19:17:10 +0300

    jquery imported

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import jQuery from 'jquery';
 
 import './index.css';
 import App from './App';
+import {menuHover} from './menu';
 // import registerServiceWorker from './registerServiceWorker';
 
 
 jQuery(document).ready(function() {
     ReactDOM.render(<App />, document.getElementById('root'));
-    alert("helo!");
+    menuHover();
 });
 
 // registerServiceWorker();
