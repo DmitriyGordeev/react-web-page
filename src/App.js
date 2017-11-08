f05017633121eb521c346e1699239c618bcf9640
Weekend Master <weekend@super.com>
2017-11-09 00:56:21 +0300

    lost ActionBar.css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
-import jQuery from 'jquery';
 
 import './App.css';
 import Grid from "./Grid";
 
-class App extends React.Component {
+class ActionBar extends React.Component {
 
     onClickHandler(e) {
-        // TODO: remove jquery?
         jQuery("#top-menu-dropdown").toggle();
     }
 
     render() {
         return (
             <div>
-                <header>
-                    <div className={"action-bar"}>
-                        <div className={"dropdown-menu"} onClick={this.onClickHandler}>
-                            <p>Тип события</p>
-                            <ul id={"top-menu-dropdown"}>
-                                <li>Item1</li>
-                                <li>Item2</li>
-                                <li>Item3</li>
-                                <li>Item4</li>
-                            </ul>
-                        </div>
+                <div className={"action-bar"}>
+                    <div className={"dropdown-menu"} onClick={this.onClickHandler}>
+                        <p>Тип события</p>
+                        <ul id={"top-menu-dropdown"}>
+                            <li>Item1</li>
+                            <li>Item2</li>
+                            <li>Item3</li>
+                            <li>Item4</li>
+                        </ul>
                     </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <ActionBar />
                 </header>
                 <Grid />
             </div>
