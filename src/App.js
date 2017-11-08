1c779996ce29e3ab025a2cd55be6118442359f8a
Weekend Master <weekend@super.com>
2017-11-09 01:12:18 +0300

    actionbar component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { Component } from 'react';
-import jQuery from 'jquery';
 
 import './App.css';
 import Grid from "./Grid";
-
-class ActionBar extends React.Component {
-
-    onClickHandler(e) {
-        jQuery("#top-menu-dropdown").toggle();
-    }
-    render() {
-        return (
-            <div className={"action-bar"}>
-                <div className={"dropdown-menu"} onClick={this.onClickHandler}>
-                    <p>Тип события</p>
-                    <ul id={"top-menu-dropdown"}>
-                        <li>Item1</li>
-                        <li>Item2</li>
-                        <li>Item3</li>
-                        <li>Item4</li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-}
+import ActionBar from "./ActionBar";
 
 class App extends React.Component {
 
