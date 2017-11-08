242b21ad96f338b0542e0d98335399731e4990e7
Weekend Master <weekend@super.com>
2017-11-08 10:26:28 +0300

    solved dynmic

--- src/Grid.js
+++ src/Grid.js
@@ -18,13 +18,51 @@ class GridRow extends React.Component {
 
 class Grid extends React.Component {
 
+    test_jsonData() {
+        var data = [
+            {
+                id: 1,
+                type: 'seminars_workshops_training',
+                title: 'Ридинг-группы',
+                location: 'Библиотека Музея «Гараж»',
+                image_url: 'https://cdn-static-garagemca.gcdn.co/storage/event/1/6/1622/preview_preview_image-288dd5b0-b91a-411f-a227-8deb39f276d0.jpg',
+                start_date: '2017-09-12',
+                end_date: '2017-11-30'
+            },
+            {
+                id: 2,
+                type: 'seminars_workshops_training',
+                title: 'Teens Day',
+                location: 'Музей современного искусства «Гараж»',
+                image_url: 'https://cdn-static-garagemca.gcdn.co/storage/event/1/6/1681/preview_preview_image-f9096671-ffcc-4a21-8757-56bedc3aa296.jpg',
+                start_date: '2017-10-01',
+                end_date: '2017-11-05'
+            },
+            {
+                id: 3,
+                type: 'seminars_workshops_training',
+                title: 'Семинар «Читаем и изучаем “Искусство с 1900 года” вместе»',
+                location: 'Образовательный центр Музея «Гараж»',
+                image_url: 'https://cdn-static-garagemca.gcdn.co/storage/event/1/6/1611/preview_preview_image-638bfea3-5e57-4df7-ad9e-3d3a8e826afa.jpg',
+                start_date: '2017-10-07',
+                end_date: '2017-12-23'
+            }
+        ];
+
+        return data;
+    }
+
+    jsonCards(data) {
+        var result = data.map(function(e) {
+            return <Card imageSrc={e.image_url} />;
+        });
+        return result;
+    }
+
     render() {
         return (
             <div>
-                <GridRow/>
-                <GridRow/>
-                <GridRow/>
-                <GridRow/>
+                { this.jsonCards(this.test_jsonData()) }
             </div>
         );
     }
