5d181be5b8112ab85040a74dc57b96b2e1e15dcb
Weekend Master <weekend@super.com>
2017-11-09 01:32:57 +0300

    filled top menu

--- src/ActionBar.js
+++ src/ActionBar.js
@@ -5,19 +5,36 @@ import './ActionBar.css';
 
 class ActionBar extends React.Component {
 
-    onClickHandler(e) {
+    test_menuJson() {
+        return {
+            seminars_workshops_training: 'Семинары, воркшопы и тренинги',
+            lectures_talks: 'Лекции и дискуссии',
+            concerts_performances: 'Концерты, спектакли и перформансы',
+            film_screenings: 'Кинопоказы'
+        };
+    }
+
+    menuList(data) {
+        var component = [];
+        for(var p in data) {
+            if (data.hasOwnProperty(p))
+                component.push(<li>{data[p]}</li>);
+        }
+
+        return component;
+    }
+
+    onClickHandler() {
         jQuery("#top-menu-dropdown").toggle();
     }
+
     render() {
         return (
             <div className={"action-bar"}>
                 <div className={"dropdown-menu"} onClick={this.onClickHandler}>
                     <p>Тип события</p>
                     <ul id={"top-menu-dropdown"}>
-                        <li>Item1</li>
-                        <li>Item2</li>
-                        <li>Item3</li>
-                        <li>Item4</li>
+                        { this.menuList(this.test_menuJson()) }
                     </ul>
                 </div>
             </div>
