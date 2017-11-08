25ef11c3de8723ed75df3de0d79fa3d1cf29a373
Weekend Master <weekend@super.com>
2017-11-08 19:40:08 +0300

    display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import jQuery from 'jquery';
+
 import './App.css';
 import Grid from "./Grid";
 
 class App extends React.Component {
+
+    onClickHandler() {
+        jQuery("#top-menu-dropdown").toggle();
+    }
 
     render() {
         return (
             <div>
                 <header>
                     <div className={"action-bar"}>
-                        <div className={"dropdown-menu"}>
+                        <div className={"dropdown-menu"} onClick={this.onClickHandler}>
                             <p>Тип события</p>
-                            <ul>
+                            <ul id={"top-menu-dropdown"}>
                                 <li>Item1</li>
                                 <li>Item2</li>
                                 <li>Item3</li>
