3f1b3cbfb2ff537e71b6e696d04e857b768f5ef3
Weekend Master <weekend@super.com>
2017-11-08 19:35:26 +0300

    onClickHandler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+
 import './App.css';
 import Card from "./Card";
 import Grid from "./Grid";
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    onClickHandler() {
+        var menu = document.getElementById("top-dropdown");
+        menu.style.display = "block";
+    }
+
     render() {
         return (
             <div>
                 <header>
                     <div className={"action-bar"}>
-                        <div className={"dropdown-menu"}>
+                        <div className={"dropdown-menu"} onClick={this.onClickHandler}>
                             <p>Тип события</p>
-                            <ul>
+                            <ul id={"top-dropdown"}>
                                 <li>Item1</li>
                                 <li>Item2</li>
                                 <li>Item3</li>
