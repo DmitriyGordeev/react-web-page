9e14e8ccf0fa69e1c338a9bf398144110c8f4150
Weekend Master <weekend@super.com>
2017-11-08 10:38:54 +0300

    grid

--- src/Grid.js
+++ src/Grid.js
@@ -46,6 +46,15 @@ class Grid extends React.Component {
                 image_url: 'https://cdn-static-garagemca.gcdn.co/storage/event/1/6/1611/preview_preview_image-638bfea3-5e57-4df7-ad9e-3d3a8e826afa.jpg',
                 start_date: '2017-10-07',
                 end_date: '2017-12-23'
+            },
+            {
+                id: 4,
+                type: 'seminars_workshops_training',
+                title: 'Семейные дни в Музее',
+                location: 'Музей современного искусства «Гараж»',
+                image_url: 'https://cdn-static-garagemca.gcdn.co/storage/event/1/6/1691/preview_preview_image-aef6db15-ee8b-4980-9392-bd0efe9c4512.jpg',
+                start_date: '2017-10-08',
+                end_date: '2017-12-10'
             }
         ];
 
@@ -54,16 +63,19 @@ class Grid extends React.Component {
 
     jsonCards(data) {
         var result = data.map(function(e) {
-            return <Card imageSrc={e.image_url} />;
+            return <Card
+                eventName={e.title}
+                eventLocation={e.location}
+                imageSrc={e.image_url} />;
         });
         return result;
     }
 
     render() {
         return (
-            <div>
+            <section className={"grid"}>
                 { this.jsonCards(this.test_jsonData()) }
-            </div>
+            </section>
         );
     }
 
